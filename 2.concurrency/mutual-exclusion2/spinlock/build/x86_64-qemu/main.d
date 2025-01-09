c54fd3bbd782f170722066dc59a4c5555d4a3723
JAILuo <1670756477@qq.com>
2025-01-09 23:22:21 +0800

    update mutual-exclusion2

---
+++ 2.concurrency/mutual-exclusion2/spinlock/build/x86_64-qemu/main.d
@@ -0,0 +1,8 @@
+/home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/build/x86_64-qemu/main.o: \
+ /home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/main.c \
+ /home/jai/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/jai/ysyx-workbench/abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/jai/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/jai/ysyx-workbench/abstract-machine/klib/include/klib.h \
+ /home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/include/spinlock.h \
+ /home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/include/kernel.h
---
+++ 2.concurrency/mutual-exclusion2/spinlock/build/x86_64-qemu/spinlock.d
@@ -0,0 +1,8 @@
+/home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/build/x86_64-qemu/spinlock.o: \
+ /home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/spinlock.c \
+ /home/jai/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/jai/ysyx-workbench/abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/jai/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/include/spinlock.h \
+ /home/jai/os-examples/2.concurrency/mutual-exclusion2/spinlock/include/kernel.h \
+ /home/jai/ysyx-workbench/abstract-machine/klib/include/klib.h
